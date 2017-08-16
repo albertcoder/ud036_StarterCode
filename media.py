82b98eb2fabfb60153c7e0220b185227756897d5
hobbyist <you@example.com>
2017-08-16 16:55:16 -0700

    Updated media.py for cleaner code as suggested by PEP8

--- media.py
+++ media.py
@@ -1,8 +1,9 @@
 import webbrowser
 
+
 # definition of class Movie which has the following instance variables
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):  # NOQA
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
